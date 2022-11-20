47416c345f0f100ccc8cbb6db230137872f503db
Cody <cody.tian@icloud.com>
2022-11-20 11:11:09 -0800

    add VMWriter enums and constructor

--- VMWriter.py
+++ VMWriter.py
@@ -1,2 +1,52 @@
+from enum import Enum
+
+
+class Segments(Enum):
+    CONST = 0
+    ARG = 1
+    LOCAL = 2
+    STATIC = 3
+    THIS = 4
+    THAT = 5
+    POINTER = 6
+    TEMP = 7
+
+
+class Command(Enum):
+    ADD = 0
+    SUB = 1
+    NEG = 2
+    EQ = 3
+    GT = 4
+    LT = 5
+    AND = 6
+    OR = 7
+    NOT = 8
+
+
 class VMWriter:
     def __init__(self, output):
+        self.output = open(output, 'w')
+        # now we need to map the segment enums to VM segments
+        self.segmentEnumToVMSegmentMapping = {
+            Segments.CONST: 'constant',
+            Segments.ARG: 'arg',
+            Segments.LOCAL: 'local',
+            Segments.STATIC: 'static',
+            Segments.THIS: 'this',
+            Segments.THAT: 'that',
+            Segments.POINTER: 'pointer',
+            Segments.TEMP: 'temp'
+        }
+        # plus, we need to map the command enums to VM arithmetic logic commands
+        self.commandEnumToVMArithmeticLogicCommandMapping = {
+            Command.ADD: 'add',
+            Command.SUB: 'sub',
+            Command.NEG: 'neg',
+            Command.EQ: 'eq',
+            Command.GT: 'gt',
+            Command.LT: 'lt',
+            Command.AND: 'and',
+            Command.OR: 'or',
+            Command.NOT: 'not'
+        }
--- jackAnalyzer.py
+++ jackAnalyzer.py
@@ -1,9 +1,16 @@
 from symbolTable import *
+from VMWriter import *
+
 # from jackTokenizer import *
 # from compilationEngine import *
 # file = 'Average/Main.jack'
 # tokenizer = JackTokenizer(file)
 # compileEngine = CompilationEngine(file, tokenizer)
 symbolTable = SymbolTable()
-print(VarType.STATIC, VarType.FIELD, VarType.ARG, VarType.VAR, VarType.NONE)
-print(symbolTable.classSymbolTable, symbolTable.subroutineSymbolTable, symbolTable.classLevelFieldCount, symbolTable.classLevelStaticCount, symbolTable.subroutineLevelArgCount, symbolTable.subroutineLevelLocalCount)
+VMWriter = VMWriter('Planning/test.txt')
+print(Segments.CONST, Segments.ARG, Segments.LOCAL, Segments.STATIC,
+      Segments.THIS, Segments.THAT, Segments.POINTER, Segments.TEMP,
+      Command.ADD, Command.SUB, Command.NEG, Command.EQ, Command.GT, Command.LT,
+      Command.AND, Command.OR, Command.NOT)
+print(VMWriter.output)
+print(VMWriter.segmentEnumToVMSegmentMapping, VMWriter.commandEnumToVMArithmeticLogicCommandMapping)
